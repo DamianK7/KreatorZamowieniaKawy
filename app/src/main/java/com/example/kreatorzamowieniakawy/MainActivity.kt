ce3b514a74291b8a0501cff907e6457cdd40379d
damian <sjihf@gmail.com>
2025-03-14 14:32:04 +0100

    first commit

---
+++ app/src/main/java/com/example/kreatorzamowieniakawy/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.kreatorzamowieniakawy
+
+import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+        val radioGroup = findViewById<RadioGroup>(R.id.meal_radiogroup)
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = findViewById<RadioButton>(checkedId)
+            val myTextView = null
+            myTextView.text = "Wybrano: ${radioButton.text}"
+        }
+    }
+
+}
