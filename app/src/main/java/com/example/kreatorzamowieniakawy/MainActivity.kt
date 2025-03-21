6dc9c03bcb6815a4c7e1502a1b5900b01c953f60
damian <sjihf@gmail.com>
2025-03-21 14:26:36 +0100

    Modyfikacja wyglądu aplikacji dodając seekbar oraz button

--- app/src/main/java/com/example/kreatorzamowieniakawy/MainActivity.kt
+++ app/src/main/java/com/example/kreatorzamowieniakawy/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.kreatorzamowieniakawy
 import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,13 +21,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 
+
         }
-        val radioGroup = findViewById<RadioGroup>(R.id.meal_radiogroup)
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            val radioButton = findViewById<RadioButton>(checkedId)
-            val myTextView = null
-            myTextView.text = "Wybrano: ${radioButton.text}"
-        }
+
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+        val myTextView = findViewById<TextView>(R.id.myTextView)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                myTextView.text = "Ilość kaw: $progress"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(this@MainActivity, "Zacząłeś przesuwać suwak", Toast.LENGTH_SHORT).show()
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                Toast.makeText(this@MainActivity, "Zakończyłeś przesuwanie suwaka", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
 }
